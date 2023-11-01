7d88751b62c4d51e7a1dc260d3e4aba5b86d2c4c
vky-fsd <vk206vignesh@gmail.com>
2023-11-01 21:15:19 +0530

    changes

--- middleware/otp.middleware.js
+++ middleware/otp.middleware.js
@@ -1,16 +1,27 @@
-const jwt=require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
- let otpverify=(req,res,next)=>{
-    let token=req.headers.authorization;
-    
-    if(token){
-        let decoded=jwt.verify(token, process.env.access_token);
-       // console.log(decoded)
-        if(decoded.Useremail){
-            req.body.Useremail=decoded.Useremail;
-          //  console.log(req.body);
-            next();
-        }  
-}
- }
-module.exports=otpverify
+let otpverify = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (token) {
+    try {
+      let decoded = jwt.verify(token, process.env.access_token);
+      // Verify the token using the correct secret/key
+      if (decoded.Useremail) {
+        req.body.Useremail = decoded.Useremail;
+        next();
+      } else {
+        // Handle the case where the Useremail is missing in the token
+        res.status(401).json({ error: 'Invalid token: Useremail is missing' });
+      }
+    } catch (err) {
+      // Handle token verification errors
+      res.status(401).json({ error: 'Invalid token' });
+    }
+  } else {
+    // Handle the case where there is no token provided
+    res.status(401).json({ error: 'Token is missing' });
+  }
+};
+
+module.exports = otpverify;
